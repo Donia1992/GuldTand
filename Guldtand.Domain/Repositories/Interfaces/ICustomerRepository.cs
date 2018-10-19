6fb9b756ed5bc96e7b9683e47f245d09bfbe8f59
Katarina Lindhed <katta36@hotmail.com>
2018-10-19 15:19:08 +0200

    Add GetAllCustomers()

---
+++ Guldtand.Domain/Repositories/Interfaces/ICustomerRepository.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+using Guldtand.Domain.Models;
+
+namespace Guldtand.Domain.Repositories
+{
+    public interface ICustomerRepository
+    {
+        List<ICustomer> GetAllCustomers();
+    }
+}

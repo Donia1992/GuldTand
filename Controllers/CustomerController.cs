c6b97c1c1a2b677c7cf68958dd216643b3170010
Katarina Lindhed <katta36@hotmail.com>
2018-10-19 12:34:24 +0200

    Adderade Customer API - Se Kund Uppgifter Fullständigt namn, personnummer, telefonnummer, email, adress, försäkrad eller ej
    Added Customer to Models, Repositories in Guldtand.Domain 
    + Changes in CustomerController.cs, Startup.cs, launchSettings.json  to list Customer in GuldtandAPI

---
+++ Controllers/CustomerController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+using Guldtand.Domain.Repositories;
+using System.Net;
+
+namespace GuldtandApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CustomerController : ControllerBase
+    {
+        private readonly ICustomerRepository _customerRepository;
+
+        public CustomerController(ICustomerRepository customerRepository)
+        {
+            _customerRepository = customerRepository;
+        }
+
+        [HttpGet]
+        [Route("")]
+        public IActionResult GetAllCustomers()
+        {
+            try
+            {
+                var customerList = _customerRepository.GetAllCustomers();
+                return Ok(customerList);
+            }
+            catch (ArgumentException exception)
+            {
+                Console.WriteLine($"{DateTime.Now:yyyy-MM-dd HH:mm:ss}: " + $"ArgumentException details: {exception.Message}");
+
+                return StatusCode((int)HttpStatusCode.BadRequest);
+            }
+            catch (Exception exception)
+            {
+                Console.WriteLine($"{DateTime.Now:yyyy-MM-dd HH:mm:ss}: " + $"Exception details: {exception.Message}");
+                return StatusCode((int)HttpStatusCode.InternalServerError);
+            }
+            finally
+            {
+                Console.WriteLine();
+            }
+        }
+    }
+}

901d31c692c4d0528c4d8c8ed3033125e8993b2f
Elenore Knutsson <elenoreknutsson@gmail.com>
2018-10-24 12:35:18 +0200

    Add migration properties to employee table

--- Guldtand.Data/Employee.cs
+++ Guldtand.Data/Employee.cs
@@ -10,14 +10,29 @@ namespace Guldtand.Data
         public int Id { get; set; }
 
         [Required]
-        [MaxLength(255)]
-        public string Name { get; set; }
-
-        [Required]
         public int RoleId { get; set; }
 
         [ForeignKey("RoleId")]
         public virtual Role Role { get; set; }
+
+        [Required]
+        [StringLength(15, MinimumLength = 2)]
+        public virtual string FirstName { get; set; }
+
+        [Required]
+        [StringLength(15, MinimumLength = 2)]
+        public virtual string LastName { get; set; }
+
+        [Required]
+        [EmailAddress]
+        public virtual string Email { get; set; }
+
+        [Required]
+        [Phone]
+        public virtual string PhoneNumber { get; set; }
+
+        [Required]
+        public virtual string Password { get; set; }
     }
 
     public class Role
---
+++ Guldtand.Data/Migrations/20181024102114_addPropertiesToEmployee.cs
@@ -0,0 +1,76 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Guldtand.Data.Migrations
+{
+    public partial class addPropertiesToEmployee : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Name",
+                table: "Employees");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Email",
+                table: "Employees",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "FirstName",
+                table: "Employees",
+                maxLength: 15,
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "LastName",
+                table: "Employees",
+                maxLength: 15,
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Password",
+                table: "Employees",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "PhoneNumber",
+                table: "Employees",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Email",
+                table: "Employees");
+
+            migrationBuilder.DropColumn(
+                name: "FirstName",
+                table: "Employees");
+
+            migrationBuilder.DropColumn(
+                name: "LastName",
+                table: "Employees");
+
+            migrationBuilder.DropColumn(
+                name: "Password",
+                table: "Employees");
+
+            migrationBuilder.DropColumn(
+                name: "PhoneNumber",
+                table: "Employees");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Name",
+                table: "Employees",
+                maxLength: 255,
+                nullable: false,
+                defaultValue: "");
+        }
+    }
+}
